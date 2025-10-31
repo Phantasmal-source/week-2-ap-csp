b0ed5976bdce679a55c1c710168b151b3b5a145d
Phantasmal-source <joahuawoodward@gmail.com>
2025-10-31 17:40:48 +0000

    bell ringer

--- week_11_day2.py
+++ week_11_day2.py
@@ -9,31 +9,31 @@ print("You’ll review variables, strings, numbers, and print formatting.\n")
 
 # Step 1: Create Variables
 # TODO: Replace the values below with your own info
-first_name = "Marvin"
-age = 30
-favorite_color = "Blue"
-favorite_number = 7
-
+first_name = "Joshua"
+age = 17
+favorite_color = "purple"
+favorite_number = 4
+result = age + favorite_number
 #  Step 2: Practice String Operations
 # 1. Print your name in uppercase
-
+print("Uppercase:", first_name.upper())
 
 # 2. Print how many letters are in your name
 
-
+print("Length of name:", len(first_name))
 # 3. Combine your name and favorite color into one message
 
 
 #  Step 3: Math Practice
 # Use arithmetic operators with your favorite number
-
+print("Your age plus your favorite number is", result)
 
 #  Step 4: User Input Practice
 # Ask the user two questions and combine answers
+food = print(input("What is your favorite food:"))
+dressing = print(input("What is your favorite dressing:"))
 
-
-
-
+print(dressing, food))
 # ⚙️ Step 5: Final Challenge (combine it all)
 # Use math and strings together
 
