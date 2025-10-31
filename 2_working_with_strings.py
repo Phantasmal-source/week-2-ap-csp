7147a44677b4a3a80016e8a4bcb9beb6296bd202
Phantasmal-source <joahuawoodward@gmail.com>
2025-10-31 17:51:15 +0000

    cool

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -87,6 +87,8 @@ sentence = "Python is fun to learn"
 # .split(): Splits the string into a list of words
 words = sentence.split()
 print("Split result:", words)
+words2 = sentence.join(words)
+print(words2)
 
 # .format(): Allows inserting values into strings using {}
 name = "Marvin"
